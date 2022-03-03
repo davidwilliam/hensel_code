242b3a74a2ce30a3dc8aa5bbad9d97c585f605dc
David William Silva <contact@davidwsilva.com>
2022-03-03 09:52:48 -0700

    add codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "hensel_code"
 
